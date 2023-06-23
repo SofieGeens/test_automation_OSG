deae63bcfd00348a153cc0b63d9327fa0fa0ee83
Sofie(Els) <sofie.geens@student.kuleuven.be>
2023-06-23 14:51:49 +0200

    minor bug fix

--- moveFiles.py
+++ moveFiles.py
@@ -3,17 +3,13 @@ import os
 
 def emptyFolder():
 	files = os.listdir(sets.protocolsUse) #get all files form the folder
-	try:
-		#remove the samples folder, otherwise the next step doesn't work
-		print("a")
-		os.rmdir(sets.sampleFolder)
-	except:
-		print("b")
-		#pass
 	for f in files:
 		sourcePath = os.path.join(sets.protocolsUse,f)				#complete the path to include the filename
 		destinationPath = os.path.join(sets.protocolsStorage,f)
-		os.rename(sourcePath,destinationPath)						#rename moves the files from sourcePath to destinationPath
+		try:
+			os.rename(sourcePath,destinationPath)						#rename moves the files from sourcePath to destinationPath
+		except:
+			pass
 
 def moveForUse(fname):
 	sourcePath = os.path.join(sets.protocolsStorage,fname)
